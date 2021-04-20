def75645d7ea65d0e8a0c31053ec39c40778990f
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-04-20 16:17:49 +0200

    add Filtering Pagination and Searching

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 
 const errorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 //Create new product =>  /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -12,13 +13,21 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-//Get All products => /api/v1/products
+//Get All products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const resPerPage = 4;
+  const productCount = await Product.countDocuments();
+
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resPerPage);
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
     count: products.length,
+    productCount,
     products,
   });
 });
