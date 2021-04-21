82287b6db95378f507bc99989e35ea004a04db2c
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-04-21 22:54:37 +0200

    Add Basic FrontEnd and Layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Home from "./components/Home";
+import { HelmetProvider } from "react-helmet-async";
+
 import "./App.css";
 
-function App() {
+const App = () => {
+  const helmetContext = {};
   return (
-    <div className="App">
-      <h1>Shop It</h1>
-    </div>
+    <HelmetProvider context={helmetContext}>
+      <div className="App">
+        <Header />
+        <Router>
+          <div className="container container-fluid">
+            <Route path="/" component={Home} exact />
+          </div>
+        </Router>
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
-}
+};
 
 export default App;
