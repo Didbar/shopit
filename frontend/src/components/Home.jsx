75d0d34fe01b6d3c406232e78e80c9efb5044595
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-04-24 16:32:29 +0200

    Add Pagination

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import MetaData from "./layouts/MetaData";
 import Product from "./product/Product";
@@ -7,18 +7,27 @@ import Loader from "./layouts/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
 import { useAlert } from "react-alert";
+import Pagination from "react-js-pagination";
 
 const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const dispatch = useDispatch();
   const alert = useAlert();
-  const { loading, products, error } = useSelector((state) => state.products);
+  const { loading, products, error, productsCount, resPerPage } = useSelector(
+    (state) => state.products
+  );
+
+  function setCurrentPageNo(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
 
   useEffect(() => {
     if (error) {
       return alert.error(error);
     }
-    dispatch(getProducts());
-  }, [dispatch, error, alert]);
+    dispatch(getProducts(currentPage));
+  }, [dispatch, error, alert, currentPage]);
 
   return (
     <Fragment>
@@ -38,6 +47,20 @@ const Home = () => {
                 ))}
             </div>
           </section>
+          {resPerPage < productsCount && (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                lastPageText={"Last"}
+                firstPageText={"First"}
+                itemClass="page-item"
+                linkClass="page-link"
+              />
+            </div>
+          )}
         </Fragment>
       )}
     </Fragment>
